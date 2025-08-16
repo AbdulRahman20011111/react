d96a915e49bda191a8a8a2f8d49446fd74733bec
AbdulRahman20011111 <mohammedabdulrahmanhabeeb@gmail.com>
2025-08-16 07:57:13 +0530

    day3

--- app.js
+++ app.js
@@ -2,4 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<h1>Hello, HI React with Parcel!</h1>);
+const heading=React.createElement("h1",{id:"heading"},"hi react");
+const JsxHeading=()=>(<h1 id="heading">hi react jsx</h1>);
+const no=1000;
+const HeadingComponent=()=>{
+    return ( <div>
+    <h2>{no+99}</h2>
+    <JsxHeading/>
+        <h1>hi functional component</h1>
+    </div> );
+}
+root.render(<HeadingComponent/>);
